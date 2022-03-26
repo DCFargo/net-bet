3ca553c6735a42fceb2b4b27b262ee99f3206539
Andrew Fargo <drew.fargo@gmail.com>
2022-03-25 21:22:17 -0500

    Work on main, webscrape

--- main.py
+++ main.py
@@ -18,14 +18,62 @@ players = []
 for i in range(num_players):
   players.append(0)  
 
-# Select game
+# Select date
+print("\n")
+while True:
+  try:
+    year = int(input("Enter year (YYYY): "))
+    if year == 2021 or year == 2022:
+      break
+    print("Please enter a year from this season. (2021-2022)")
+  except ValueError:
+    print("Please enter a valid year. (YYYY)")
 
+while True:
+  try:
+    month = int(input("Month (MM): "))
+    if month <= 4 and month >= 10 and month > 0 and month < 13:
+      break
+    print("Please enter amonth from this season (Oct 2021, Apr 2022")
+  except ValueError:
+    print("Please enter a valid month. (MM)")
+
+while True:
+  try:
+    day = int(input("Day (DD): "))
+    if day > 0 and day < 32:
+      break
+    print("Please enter a valid day. (DD)")
+  except ValueError:
+    print("Please enter a valid day. (DD)")
+    
+game_date = str(year) + "-" + str(month) + "-" + str(day)
+
+# List game teams
+
+team_array = webscrape.teams_on_day(game_date)
+for i in range(len(team_array)):
+  print(str(i + 1) + ": " + team_array[i])
 print("\n")
 
+while True:
+  try:
+    team_index = int(input("Enter team index (number): "))
+    if team_index >= 0 and team_index < len(team_array):
+      break
+    print("Please enter a valid team index. 1-" + str(len(team_array)))
+  except ValueError:
+    print("Please enter a number.")
+
+team_str = team_array[team_index - 1]
 
 # Webscrape all info
 
+scraper = webscrape.Scraper(game_date, team_str)
+
 # Show users hint info
+
+
 
 # Player bets
 
--- webscrape.py
+++ webscrape.py
@@ -1,4 +1,4 @@
-import requests, bs4
+import requests, bs4, pandas
 
 class Scraper:
     #date should be in format of '2022-03-16'
@@ -7,23 +7,36 @@ class Scraper:
         #Gets page from url, gets the text, then makes a Beautiful soup object with it for parsing
         self.game_site = bs4.BeautifulSoup(requests.get(self.game_url).text)
         
-        #Gets the elements from the game site that have the urls to the teams
-        elems = self.game_site.select('.text-fg')
-        urls = list(set([x['href'] for x in elems]))
+        #Gets the elements from the page, then make self.team1site and team2site from them
         
-        #Make the url and BeautifulSoup for each team
-        self.team1_url = 'https://plaintextsports.com' + urls[0]
-        self.team2_url = 'https://plaintextsports.com' + urls[1]
+        elems = self.game_site.select('.text-fg')
+        urls = list(set([x[href] for x in elems]
+
+
+        self.team1_url = ''
+        self.team2_url = ''
         self.team1_site = bs4.BeautifulSoup(requests.get(self.team1_url).text)
         self.team2_site = bs4.BeautifulSoup(requests.get(self.team2_url).text)
-
+        #REMEMBER TO RETURN STRINGS
     def get_plays(self):
         # getting play-by-play
         self.game_site.select('.play-by-play-toggle-content')
+        self.plays = pandas.DataFrame()
         # q-play-by-play
     def get_team_record(self):
+        # calculates the team record before the game
         
-    # this is for later
+    def get_winner(self):
+        
+    def get_mvp(self):
+        
+    def get_lvp(self):
+        
+    def get_team_record(self):
+                 
+    def get_dumb_box_score_info(self):
+        #this is weird and pain so we deal with it later
+    # returns array of strings corr to teams
     @staticmethod
     def teams_on_day(date):
         url = 'https://plaintextsports.com/nba/' + date

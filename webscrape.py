144b68185615e5fbd19cd3d9ac015f2cb6aafb36
Andrew Fargo <drew.fargo@gmail.com>
2022-03-25 20:53:45 -0500

    initial vscode liveshare push
    Co-authored-by: Peter <Lact0@users.noreply.github.com>
    Co-authored-by: SXK <Snipexkillo@users.noreply.github.com>
    Co-authored-by: Anish Sheth <anishsheth05@gmail.com>

--- webscrape.py
+++ webscrape.py
@@ -6,13 +6,23 @@ class Scraper:
         self.game_url = 'https://plaintextsports.com/nba/' + date + '/' + teams
         #Gets page from url, gets the text, then makes a Beautiful soup object with it for parsing
         self.game_site = bs4.BeautifulSoup(requests.get(self.game_url).text)
-        #Get teams from the page, then make self.team1site and team2site from them
         
-        self.team1_url = ''
-        self.team2_url = ''
+        #Gets the elements from the game site that have the urls to the teams
+        elems = self.game_site.select('.text-fg')
+        urls = list(set([x['href'] for x in elems]))
+        
+        #Make the url and BeautifulSoup for each team
+        self.team1_url = 'https://plaintextsports.com' + urls[0]
+        self.team2_url = 'https://plaintextsports.com' + urls[1]
         self.team1_site = bs4.BeautifulSoup(requests.get(self.team1_url).text)
         self.team2_site = bs4.BeautifulSoup(requests.get(self.team2_url).text)
 
+    def get_plays(self):
+        # getting play-by-play
+        self.game_site.select('.play-by-play-toggle-content')
+        # q-play-by-play
+    def get_team_record(self):
+        
     # this is for later
     @staticmethod
     def teams_on_day(date):
